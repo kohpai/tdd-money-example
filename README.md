a2b01f8e4be4485f5add7338877899e528ae7841
Kohpai <whcwhc78@gmail.com>
2022-10-07 23:54:25 +0200

    :tada:

---
+++ README.md
@@ -0,0 +1,9 @@
+# TDD - Money Example using Kotlin
+
+## TODO
+
+- $5 + 10 CHF = $10 if rage is 2:1
+- ~~**$5 * 2 = $10**~~
+- Make `amount` private
+- Dollar side effects?
+- Money rounding?
---
+++ src/test/kotlin/MoneyTest.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MoneyTest {
+    @Test
+    fun testMultiplication() {
+        val five = Dollar(5)
+        five.times(2)
+        assertEquals(10, five.amount)
+    }
+}
